e8ecbf2a32414289dcd79192b9e9d40053ff2df6
galina-dmitrishin <67236906+galina-dmitrishin@users.noreply.github.com>
2021-06-28 10:54:44 +0300

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+setTimeout(function() {
+    document.querySelector('.circle__text1').style.display = 'block';
+ }, 3000);
+
+setTimeout(function() {
+   document.querySelector('.circle__text2').style.display = 'block';
+}, 2500);
+
+setTimeout(function() {
+   document.querySelector('.circle__text3').style.display = 'block';
+}, 2000);
+
+setTimeout(function() {
+   document.querySelector('.circle__text4').style.display = 'block';
+}, 1500);
+
+setTimeout(function() {
+   document.querySelector('.circle__text5').style.display = 'block';
+}, 1000);
+
+ 
